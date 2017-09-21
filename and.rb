e02903b6c7207bd0efd6ba7910c3ee5cbb80029d
Ryan Hageman <improv42@gmail.com>
2017-09-21 09:13:52 -0500

    Add comments from the future

--- and.rb
+++ and.rb
@@ -6,6 +6,10 @@ else
   puts "Too little,  too much."
 end
 
+# These are some comments from the future.
+# They only live in the future, you can't see me, people in the past.
+
+
 # choice = "wampum"
 
 # if choice == "cash" || choice == "credit"
